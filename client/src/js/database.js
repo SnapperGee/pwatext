4f0c361c52bc17c98e38abe4314efd70e8c24fe8
Snap <XxSnapperGeexX@gmail.com>
2023-12-14 16:46:20 -0800

    Add function to save and retrieve data from index database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,41 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
+
+const DB_NAME = "pwatext";
+const OBJ_STORE_NAME = "text";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB(DB_NAME, 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains(OBJ_STORE_NAME)) {
+        console.log(`"${DB_NAME}" database with "${OBJ_STORE_NAME}" object store already exists`);
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore(OBJ_STORE_NAME, { keyPath: "id", autoIncrement: true });
+      console.log(`"${DB_NAME}" database with "${OBJ_STORE_NAME}" object store created`);
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+/**
+ * Adds string to the first index of the indexed database.
+ * @param {string} content
+ */
+export const putDb = async (content) =>
+{
+  const db = await openDB(DB_NAME, 1);
+  const keyOfUpdatedIndex = await db.put(OBJ_STORE_NAME, content, 1);
+  console.log(`Saved to database index ${keyOfUpdatedIndex}:\n"""\n${await db.get(OBJ_STORE_NAME, keyOfUpdatedIndex)}\n"""`);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+/**
+ * Gets the string content from the indexed database.
+ * @returns {Promise<string>} String content from indexed database.
+ */
+export const getDb = async () =>
+{
+  const db = await openDB(DB_NAME, 1);
+  const content = await db.get(OBJ_STORE_NAME, 1);
+  console.log(content !== undefined ? `Retrieved from database:\n"""\n${content}\n"""` : "Data not found in the database");
+  return content;
+};
 
 initdb();
